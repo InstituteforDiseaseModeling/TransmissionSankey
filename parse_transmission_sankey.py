15e42319975e761dc555c90bc5f0a955716306e7
annabershteyn <abershteyn@idmod.org>
2017-09-08 19:09:30 -0700

    Efficiency improvement: divide relationships into nodes

--- parse_transmission_sankey.py
+++ parse_transmission_sankey.py
@@ -31,14 +31,13 @@
 ## for each row of interest, look up IP list of A from RelationshipStart.csv
 
 
-
 import pandas as pd
 import os as os
 import time as time
 #from os import listdir
 #from os import path
 
-TEST_MODE = True
+TEST_MODE = False
 curr_folder = 'output_TRANSMISSION_AND_RELATIONSHIPS'
 base_directory_path = 'Z:/mint/Dropbox (IDM)/research/HIV/2017/Kenya_non_resident_comparison/modeling'
 save_transmission_dataset_filename = "Transmission_with_RiskIP.pkl"
@@ -47,55 +46,75 @@ how_often_to_report_time = 1 # number of transmission events to record before re
 script_start_time = time.time()
 progress_tracker = 0
 
+    
+if TEST_MODE & os.path.isfile('pickle_transm_input_csv.pkl') & os.path.isfile('pickle_rels_input_csv.pkl'):
+    transm = pd.read_pickle('pickle_transm_input_csv.pkl')
+    rels = pd.read_pickle('pickle_rels_input_csv.pkl')
+else:
+    # load transmission report  as a pandas data frame
+    transm = pd.read_csv(os.path.join(base_directory_path, curr_folder, 'TransmissionReport.csv'))
 
-# load transmission report  as a pandas data frame
-transm = pd.read_csv(os.path.join(base_directory_path, curr_folder, 'TransmissionReport.csv'))
+    # keep only years >=2015 and <2020
+    transm = transm[(transm.YEAR >= 2015) & (transm.YEAR < 2020)]
 
-# keep only years >=2015 and <2020
-transm = transm[(transm.YEAR >= 2015) & (transm.YEAR < 2020)]
+    # keep only relevant columns
+    transm = transm[['YEAR','NODE_ID','SRC_ID','SRC_GENDER','SRC_AGE','SRC_CIRCUMSIZED','SRC_INF_AGE', 'DEST_ID','DEST_GENDER','DEST_AGE','DEST_CIRCUMSIZED']]
 
-# keep only relevant columns
-transm = transm[['YEAR','NODE_ID','SRC_ID','SRC_GENDER','SRC_AGE','SRC_CIRCUMSIZED','SRC_INF_AGE', 'DEST_ID','DEST_GENDER','DEST_AGE','DEST_CIRCUMSIZED']]
+    # load relationship CSV
+    rels = pd.read_csv(os.path.join(base_directory_path, curr_folder, 'RelationshipStart.csv'))
+
+    # keep only relevant columns
+    rels = rels[['NODE_ID','Rel_start_time','Rel_type (0 = TRANSITORY; 1 = INFORMAL; 2 = MARITAL; 3 = COMMERCIAL)','Current_node_ID', 'A_ID', 'B_ID', 'A_IndividualProperties', 'B_IndividualProperties']]
+
+    # the model logs time only in days. Translate this to years using 1960.5 as Start_Year
+    # Note, verify that this Start_Year is correct by checking config.json
+    rels['YEAR'] = rels.Rel_start_time/365 + 1960.5
+    rels = rels[(rels.YEAR  < 2020)]
+
+    # to speed this up, could divide rels into separate datasets by node.
+    # then I'd have to look up the appropriate rels dataset.
+
+    if TEST_MODE:
+        transm.to_pickle("pickle_transm_input_csv.pkl") 
+        rels.to_pickle("pickle_rels_input_csv.pkl") 
+        
+
+rels_node = ('NOTHING',
+    rels[(rels.Current_node_ID == 1)],
+    rels[(rels.Current_node_ID == 2)],
+    rels[(rels.Current_node_ID == 3)],
+    rels[(rels.Current_node_ID == 4)],
+    rels[(rels.Current_node_ID == 5)],
+    rels[(rels.Current_node_ID == 6)],
+    );
+
 
 if TEST_MODE:
     transm = transm.head(10)
-    save_transmission_dataset_filename = "test.pkl"
+    save_transmission_dataset_filename = "test.pkl"        
 
-#global rels
-#global number_of_transmissions_to_analyze
 number_of_transmissions_to_analyze = transm.shape[0]
-
-# load relationship CSV
-rels = pd.read_csv(os.path.join(base_directory_path, curr_folder, 'RelationshipStart.csv'))
-
-# keep only relevant columns
-rels = rels[['Rel_start_time','Rel_type (0 = TRANSITORY; 1 = INFORMAL; 2 = MARITAL; 3 = COMMERCIAL)','Current_node_ID', 'A_ID', 'B_ID', 'A_IndividualProperties', 'B_IndividualProperties']]
-
-# the model logs time only in days. Translate this to years using 1960.5 as Start_Year
-# Note, verify that this Start_Year is correct by checking config.json
-rels['YEAR'] = rels.Rel_start_time/365 + 1960.5
-rels = rels[(rels.YEAR  < 2020)]
-
-#transm['SRC_Risk'] = 'SRC_TBD'
-#transm['DEST_Risk'] = 'DEST_TBD'
-
+        
 # for each transmission event in 2015-2020,
 def determine_risk_IP_for_SRC(curr_transm):
      
     iter_start_time = time.time()
     global progress_tracker 
     global script_start_time
-    global rels
     global number_of_transmissions_to_analyze
-    
+    global rels_node
+        
     # take the FROM individual ID and year of transmission event
     curr_SRC_ID = curr_transm['SRC_ID']
     curr_transm_yr = curr_transm['YEAR']
+    curr_NODE_ID = int(curr_transm['NODE_ID'])
+    #curr_node_rels = rels_node[curr_NODE_ID]
+   
     
+    # COWS SWITCH by NODE ID TO ADD GLOBAL rels FOR THIS NODE ID!!! TO DO!
     
     # assign IP to SRC individual
-    
-    SRC_rels = rels[((rels.A_ID == curr_SRC_ID) | (rels.B_ID == curr_SRC_ID))]
+    SRC_rels = rels_node[curr_NODE_ID][((rels_node[curr_NODE_ID].A_ID == curr_SRC_ID) | (rels_node[curr_NODE_ID].B_ID == curr_SRC_ID))]
     
     # determine if they have been in a COMMERCIAL relship prior to the date of transmission:
     SRC_commercial_rels = SRC_rels[(SRC_rels['Rel_type (0 = TRANSITORY; 1 = INFORMAL; 2 = MARITAL; 3 = COMMERCIAL)']==3) & (SRC_rels['YEAR'] <= curr_transm_yr)]
@@ -108,10 +127,10 @@ def determine_risk_IP_for_SRC(curr_transm):
     
         # the SRC transmitter could be either person A or person B in the relationship. 
         # If they are person A and person A's Individual Properties are Risk-MEDIUM, then they are MEDIUM risk. Call this Condition A.
-        condition_A_to_be_MEDIUM_Risk = sum((rels.A_ID == curr_SRC_ID) & (SRC_rels.A_IndividualProperties.str.contains("MEDIUM")))>0
+        condition_A_to_be_MEDIUM_Risk = sum((rels_node[curr_NODE_ID].A_ID == curr_SRC_ID) & (SRC_rels.A_IndividualProperties.str.contains("MEDIUM")))>0
         
         # If they are person B and person B's Individual Properties are Risk-MEDIUM, then they are MEDIUM risk. Call this Condition B.
-        condition_B_to_be_MEDIUM_Risk = sum((rels.B_ID == curr_SRC_ID) & (SRC_rels.B_IndividualProperties.str.contains("MEDIUM")))>0
+        condition_B_to_be_MEDIUM_Risk = sum((rels_node[curr_NODE_ID].B_ID == curr_SRC_ID) & (SRC_rels.B_IndividualProperties.str.contains("MEDIUM")))>0
         
         if condition_A_to_be_MEDIUM_Risk | condition_B_to_be_MEDIUM_Risk:
             #transm.SRC_Risk.loc[[transm_index]] = 'MEDIUM'
@@ -140,13 +159,15 @@ def determine_risk_IP_for_DEST(curr_transm):
     iter_start_time = time.time()
     global progress_tracker 
     global script_start_time
-    global rels
+    global rels_node
     global number_of_transmissions_to_analyze
     
     curr_DEST_ID = curr_transm['DEST_ID']    
     curr_transm_yr = curr_transm['YEAR']
+    curr_NODE_ID = int(curr_transm['NODE_ID'])
+   
     
-    DEST_rels = rels[((rels.A_ID == curr_DEST_ID) | (rels.B_ID == curr_DEST_ID))]
+    DEST_rels = rels_node[curr_NODE_ID][((rels_node[curr_NODE_ID].A_ID == curr_DEST_ID) | (rels_node[curr_NODE_ID].B_ID == curr_DEST_ID))]
     
     # determine if they have been in a COMMERCIAL relship prior to the date of transmission:
     DEST_commercial_rels = DEST_rels[(DEST_rels['Rel_type (0 = TRANSITORY; 1 = INFORMAL; 2 = MARITAL; 3 = COMMERCIAL)']==3) & (DEST_rels['YEAR'] <= curr_transm_yr)]
@@ -159,10 +180,10 @@ def determine_risk_IP_for_DEST(curr_transm):
     
         # the DEST transmitter could be either person A or person B in the relationship. 
         # If they are person A and person A's Individual Properties are Risk-MEDIUM, then they are MEDIUM risk. Call this Condition A.
-        condition_A_to_be_MEDIUM_Risk = sum((rels.A_ID == curr_DEST_ID) & (DEST_rels.A_IndividualProperties.str.contains("MEDIUM")))>0
+        condition_A_to_be_MEDIUM_Risk = sum((rels_node[curr_NODE_ID].A_ID == curr_DEST_ID) & (DEST_rels.A_IndividualProperties.str.contains("MEDIUM")))>0
         
         # If they are person B and person B's Individual Properties are Risk-MEDIUM, then they are MEDIUM risk. Call this Condition B.
-        condition_B_to_be_MEDIUM_Risk = sum((rels.B_ID == curr_DEST_ID) & (DEST_rels.B_IndividualProperties.str.contains("MEDIUM")))>0
+        condition_B_to_be_MEDIUM_Risk = sum((rels_node[curr_NODE_ID].B_ID == curr_DEST_ID) & (DEST_rels.B_IndividualProperties.str.contains("MEDIUM")))>0
         
         if condition_A_to_be_MEDIUM_Risk | condition_B_to_be_MEDIUM_Risk:
             #transm.DEST_Risk.loc[[transm_index]] = 'MEDIUM'
